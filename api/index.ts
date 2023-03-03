9afc0d593fcb04057aaae321ec0ccd1bc2909d8b
Vasanth Developer <vsnthdev@gmail.com>
2023-03-03 20:54:31 +0530

    ✨ create serverless function

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+    return res.status(200).json({
+        message: 'hello world'
+    })
+}
